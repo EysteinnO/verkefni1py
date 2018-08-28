75878d0c975c12904f943127cef3fd717049af8e
Eysteinn Orri Sigurðsson <eysteinnsi227@nemi.tskoli.is>
2018-08-28 14:20:33 +0000

    first commit

---
+++ verkefni1.py
@@ -0,0 +1,28 @@
+from bottle import route, run, error
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
+
+@route('/')
+def index():
+    return '''
+            <h1>Hallo heimur</h1>
+            <a href='About'>About</a> 
+            <a href='Biography'>Biography</a>
+            <a href='Pictures'>Pictures</a>
+            '''
+@route('/<pagename>')
+def pages(pagename):
+    if pagename == 'About':
+        return "This is the about page"
+    if pagename == 'Biography':
+            return "This is the biography page"
+    if pagename == 'Pictures':
+            return "This is the picture page"
+
+@route('/Pictures')
+def Pictures():
+    return "Pictures page"
+run(host='localhost', port=8080, debug=True)
+
